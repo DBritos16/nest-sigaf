3eaad409605f7415f5c83737ec8e91bbceab09e0
DBritos16 <dbriitos16@gmail.com>
2023-10-17 16:37:38 -0300

    Get cultivos
    Servicio con los respectivos joins para obtener la inforamcion del cultivo cuando con todas sus actividades y relaciones

--- src/modules/cultivos/cultivos.service.ts
+++ src/modules/cultivos/cultivos.service.ts
@@ -4,6 +4,8 @@ import { Cultivo } from './entities/cultivo.entity';
 import { Insumo } from '../insumos/entities/insumo.entity';
 import { Campana } from '../campanas/entities/campana.entity';
 import { Actividad } from '../actividades/entities/actividad.entity';
+import { UnidadDeMedida } from '../insumos/entities/unidadDeMedida.entity';
+import { Categoria } from '../insumos/entities/categoria.entity';
 
 @Injectable()
 export class CultivosService {
@@ -14,7 +16,21 @@ export class CultivosService {
       where: {
         idParcela
       },
-      include: [Insumo, Campana, Actividad]
+      include: [Insumo, Campana, {
+        model: Actividad,
+        include: [{
+          model: Insumo, 
+          attributes: ['nombre'],
+          include: [{
+            model: UnidadDeMedida,
+            attributes: ['nombre']
+          }, {
+            model: Categoria,
+            attributes: ['nombre']
+          }],
+        },
+      ]
+      }]
     });
   }
 
